a19d2f43e648082b7b095483c111aa85d3830a31
tomonari-t <tomo1208.japan@gmail.com>
2019-12-15 18:12:07 +0900

    chore(app): setup redux devtool

--- yosemite/src/App.tsx
+++ yosemite/src/App.tsx
@@ -1,25 +1,31 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React from 'react'
 import {SafeAreaView, Text, StatusBar} from 'react-native'
+import {createStore, combineReducers, applyMiddleware} from 'redux'
+import reduxThunk from 'redux-thunk'
+import {Provider} from 'react-redux'
+import {composeWithDevTools} from 'redux-devtools-extension'
+
+const increment = (state: any = {counter: 0}) => ({
+  ...state,
+  counter: state.counter + 1,
+})
+
 const App = () => {
-  console.log('ddd')
-  const usingHermes =
-    typeof HermesInternal === 'object' && HermesInternal !== null
+  let middleweres = [reduxThunk]
+  const store = createStore(
+    combineReducers({
+      increment,
+    }),
+    composeWithDevTools(applyMiddleware(...middleweres))
+  )
   return (
     <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <Text>iiii</Text>
-      </SafeAreaView>
+      <Provider store={store}>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          <Text>iiii</Text>
+        </SafeAreaView>
+      </Provider>
     </>
   )
 }
